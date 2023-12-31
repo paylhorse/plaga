4e417e2bc69390c92975eff6049966cd831a226c
paylhorse <realalsuwaidi@gmail.com>
2023-12-31 05:14:23 -0800

    Identifier and IntegerLiteral expression parsing implemented

--- lib/parser/parser.ml
+++ lib/parser/parser.ml
@@ -3,21 +3,74 @@ open Token
 open Lexer
 open Ast
 
+type precedence =
+  | Lowest
+  | Equals      (* == *)
+  | LessGreater (* > or < *)
+  | Sum         (* + *)
+  | Product     (* * *)
+  | Prefix      (* -X or !X *)
+  | Call        (* myFunction(X) *)
+
+let precedence_value = function
+  | Lowest -> 1
+  | Equals -> 2
+  | LessGreater -> 3
+  | Sum -> 4
+  | Product -> 5
+  | Prefix -> 6
+  | Call -> 7
+
+type prefix_parse_fn = unit -> expression
+type infix_parse_fn = expression -> expression
+
+module TokenType = struct
+  type t = token_type
+  let compare = compare
+end
+
+module PrefixParseFnMap = Map.Make (TokenType)
+module InfixParseFnMap = Map.Make (TokenType)
+
 type parser = {
     lexer: lexer;
     mutable curr_token: token option;
     mutable peek_token: token option;
     mutable errors: string list;
+    prefix_parse_fns: prefix_parse_fn PrefixParseFnMap.t ref;
+    infix_parse_fns: infix_parse_fn InfixParseFnMap.t ref;
 }
 
 let next_token parser =
   parser.curr_token <- parser.peek_token;
   parser.peek_token <- Some (next_token parser.lexer)
 
+let register_prefix parser token_type fn =
+  parser.prefix_parse_fns := PrefixParseFnMap.add token_type fn !(parser.prefix_parse_fns)
+
+let register_infix parser token_type fn =
+  parser.infix_parse_fns := InfixParseFnMap.add token_type fn !(parser.infix_parse_fns)
+
+let parse_identifier parser =
+  Identifier { token = Option.get parser.curr_token; value = (Option.get parser.curr_token).literal}
+
+let parse_integer_literal parser =
+  (* TODO: in_to_string error handling *)
+  IntegerLiteral { token = Option.get parser.curr_token; value = int_of_string (Option.get parser.curr_token).literal}
+
 let new_parser lexer =
-  let parser = { lexer; curr_token = None; peek_token = None; errors = []; } in
+  let parser = {
+    lexer;
+    curr_token = None;
+    peek_token = None;
+    errors = [];
+    prefix_parse_fns = ref PrefixParseFnMap.empty;
+    infix_parse_fns = ref InfixParseFnMap.empty;
+  } in
   next_token parser;
   next_token parser;
+  register_prefix parser Ident (fun () -> parse_identifier parser);
+  register_prefix parser Int (fun () -> parse_integer_literal parser);
   parser
 
 let get_errors parser =
@@ -75,13 +128,30 @@ let parse_return_statement parser =
   done;
   ReturnStatement { token; return_value = EmptyExpression }
 
+let parse_expression parser =
+  match parser.curr_token with
+  | Some { typ = Some t; _ } ->
+    (match PrefixParseFnMap.find_opt t !(parser.prefix_parse_fns) with
+    | Some prefix_fn -> prefix_fn ()
+    | None -> EmptyExpression)
+  | _ -> EmptyExpression
+
+let parse_expression_statement parser =
+  let stmt = ExpressionStatement {
+    token = Option.get parser.curr_token;
+    expression = parse_expression parser
+  } in
+  if peek_token_is parser Semicolon then next_token parser;
+  stmt
+
 let parse_statement parser =
   match parser.curr_token with
   | Some { typ = Some Bind; _ } ->
     parse_bind_statement parser
   | Some { typ = Some Return; _ } ->
     parse_return_statement parser
-  | _ -> EmptyStatement
+  | _ ->
+    parse_expression_statement parser
 
 let parse_program parser =
   let rec helper statements =
--- test/parser_test/parser_test.ml
+++ test/parser_test/parser_test.ml
@@ -45,15 +45,66 @@ let test_return_statements () =
 
   check_parser_errors parser;
 
-  let num_expected = 3  (* Expected number of return statements *)
+  let num_expected = 3
   in
   let num_statements = List.length program.statements in
   Alcotest.(check int) "Number of statements" num_expected num_statements;
 
   List.iteri (fun i stmt ->
     match stmt with
-    | ReturnStatement _ -> ()  (* This is expected, do nothing *)
+    | ReturnStatement _ -> ()
     | _ -> Alcotest.fail ("Statement " ^ string_of_int i ^ " is not a ReturnStatement")
+  ) program.statements
+
+let test_identifier_expression () =
+  let input = "foobar;"
+  in
+  let lexer = new_lexer input in
+  let parser = new_parser lexer in
+  let program = parse_program parser in
+
+  check_parser_errors parser;
+
+  let num_expected = 1
+  in
+  let num_statements = List.length program.statements in
+  Alcotest.(check int) "Number of statements" num_expected num_statements;
+
+  List.iteri (fun i stmt ->
+    match stmt with
+    | ExpressionStatement { expression; _ } ->
+      begin match expression with
+        | Identifier _ -> ()
+        | _ -> Alcotest.fail ("Statement " ^ string_of_int i ^ " is not a Identifier")
+      end
+    | _ -> Alcotest.fail ("Statement " ^ string_of_int i ^ " is not a ExpressionStatement")
+  ) program.statements
+
+let test_integer_literal_expression () =
+  let input = "4;"
+  in
+  let lexer = new_lexer input in
+  let parser = new_parser lexer in
+  let program = parse_program parser in
+
+  check_parser_errors parser;
+
+  let num_expected = 1
+  in
+  let num_statements = List.length program.statements in
+  Alcotest.(check int) "Number of statements" num_expected num_statements;
+
+  List.iteri (fun i stmt ->
+    match stmt with
+    | ExpressionStatement { expression; _ } ->
+      begin match expression with
+      | IntegerLiteral { value; _ } ->
+        begin
+          if value != 4 then Alcotest.fail ("IntegerLiteral value " ^ string_of_int value ^ " is wrong!");
+        end
+      | _ -> Alcotest.fail ("Statement " ^ string_of_int i ^ " is not a IntegerLiteral")
+      end
+    | _ -> Alcotest.fail ("Statement " ^ string_of_int i ^ " is not a ExpressionStatement")
   ) program.statements
 
 let () =
@@ -61,4 +112,6 @@ let () =
   run "Parser Tests" [
     "test_bind_statements", [test_case "Bind statements" `Quick test_bind_statements];
     "test_return_statements", [test_case "Return statements" `Quick test_return_statements];
+    "test_identifier_expression", [test_case "Identifier expression" `Quick test_identifier_expression];
+    "test_integer_literal_expression", [test_case "IntegerLiteral expression" `Quick test_integer_literal_expression];
   ]
